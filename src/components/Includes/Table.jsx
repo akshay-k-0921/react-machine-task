f5dbea3dd45ab3ce79692c7f88e85a3b1186cedb
akshay <akshay.k.0921@gmail.com>
2024-01-26 23:57:19 +0530

    pagination added

--- src/components/Includes/Table.jsx
+++ src/components/Includes/Table.jsx
@@ -4,9 +4,25 @@ import React, { useEffect, useState } from 'react'
 import api from '../AxiosConfig'
 import styled from 'styled-components';
 
+// images 
+import leftArrow from '../../Assets/left-rrow.svg'
+import rightArrow from '../../Assets/right-arrow.svg'
+
 function Table() {
 
     const [tableData, setTableData] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const indexOfLastItem = currentPage * 5;
+    const indexOfFirstItem = indexOfLastItem - 5;
+
+    const currentRows = tableData.slice(indexOfFirstItem, indexOfLastItem);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+      };
+
+    const totalPages = Math.ceil(tableData.length / 5);
 
     useEffect(() => {
         api.get('table/').then(response =>{
@@ -39,6 +55,26 @@ function Table() {
                 }
             </TableBody>
         </TableContainer>
+        {/* Pagination controls */}
+        <div className='pagination'>
+            <button className='button' onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
+            <img src={leftArrow} alt="Previous" />
+            </button>
+
+            {Array.from({ length: totalPages }, (_, index) => (
+            <button className='pageNumber button' key={index} onClick={() => handlePageChange(index + 1)}>
+                {index + 1}
+            </button>
+            ))}
+
+            <button
+            className='button'
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+            >
+            <img src={rightArrow} alt="Next" />
+            </button>
+        </div>
     </MainContainer>
   )
 }
@@ -46,6 +82,28 @@ function Table() {
 export default Table
 
 const MainContainer = styled.div`
+
+    .pagination{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+    }
+    .button{
+        padding: 5px 8px;
+        background: #fff;
+        border: none;
+        border-radius: 4px;
+    }
+    .pageNumber{
+        height: 25px;
+    }
+    img{
+        display: block;
+        width: 10px;
+        height: 15px;
+    }
+
 `
 
 const TableContainer = styled.table`
@@ -54,6 +112,7 @@ const TableContainer = styled.table`
     width: 100%;
     border-radius: 15px;
     overflow: hidden;
+    margin-bottom: 20px;
 `
 
 const TableHead = styled.thead``
